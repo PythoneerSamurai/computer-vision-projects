a51202cbe52b5852cea989e5fee3eb1f9cd4324b
MCVD <haroonrashidcc3@gmail.com>
2024-07-11 22:24:24 +0500

    Create pytorch-implementation.py

---
+++ image-classification/brain-tumor-classification/pytorch-implementation.py
@@ -0,0 +1,98 @@
+'''
+Note:
+The labels are as follows:
+1. Meningioma samples
+2. Glioma samples
+3. Pituitary tumor samples
+'''
+
+# imports
+import torch
+import torch.nn as nn
+from torchvision.datasets import ImageFolder
+from torchvision.transforms import v2
+from torch.utils.data import DataLoader
+from torchinfo import summary
+from torchmetrics import Accuracy
+
+# initializing transforms
+TRANSFORMS = v2.Compose([
+    v2.Resize((128, 128)),
+    v2.Grayscale(),
+    v2.ToTensor(),
+    v2.ToDtype(torch.float32),
+])
+
+# data loading
+RAW_DATA = ImageFolder("/kaggle/input/brain-tumor", transform=TRANSFORMS)
+LOADED_DATA = DataLoader(RAW_DATA, shuffle=True, batch_size=64)
+
+# model implementation
+class ConvNet(nn.Module):
+    def __init__(self, in_channels=1):
+        super().__init__()
+        self.sequential = nn.Sequential(
+            nn.BatchNorm2d(num_features=in_channels),
+            nn.Conv2d(in_channels=in_channels, out_channels=128, kernel_size=(3, 3), padding="same"),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), padding="same"),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2)),
+            nn.Dropout2d(0.2),
+            nn.BatchNorm2d(num_features=128),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), padding="same"),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), padding="same"),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2)),
+            nn.Dropout2d(0.2),
+            nn.BatchNorm2d(num_features=128),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), padding="same"),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), padding="same"),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2)),
+            nn.Dropout2d(0.2),
+            nn.BatchNorm2d(num_features=128),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), padding="same"),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), padding="same"),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2)),
+            nn.Dropout2d(0.2),
+            nn.Flatten(),
+            nn.Linear(in_features=128*64, out_features=512),
+            nn.ReLU(),
+            nn.Linear(in_features=512, out_features=128),
+            nn.ReLU(),
+            nn.Linear(in_features=128, out_features=32),
+            nn.ReLU(),
+            nn.Linear(in_features=32, out_features=3),
+        )
+        
+    def forward(self, x):
+        return self.sequential(x)
+
+MODEL = ConvNet().cuda()
+'''print(summary(MODEL, (32, 1, 128, 128)))'''
+
+# parameters and initializations
+EPOCHS = 20
+ACCURACY_CALCULATOR = Accuracy(task="multiclass", num_classes=3).cuda()
+LOSS_FUNCTION = nn.CrossEntropyLoss()
+OPTIMIZER = torch.optim.Adam(MODEL.parameters())
+
+# training loop and model saving
+for epoch in range(EPOCHS):
+    for iteration, (images, labels) in enumerate(LOADED_DATA):
+        images, labels = images.cuda(), labels.cuda()
+        predictions = MODEL(images)
+        loss = LOSS_FUNCTION(predictions, labels)
+        accuracy = ACCURACY_CALCULATOR(predictions, labels)
+        OPTIMIZER.zero_grad(set_to_none=True)
+        loss.backward()
+        OPTIMIZER.step()
+        if iteration % 20 == 0:
+            print(f'epoch: {epoch}, iteration: {iteration}, loss: {loss:3f}, accuracy: {accuracy:3f}')
+
+torch.save(MODEL, "brainTumor.pt")
